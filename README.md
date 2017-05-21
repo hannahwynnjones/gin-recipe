61b2a85e0b5ab690c2677613725f435fff57e567
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-05-21 22:55:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# One Page Website
+
+I wanted to play around with an angular front-end for a one page site without using bootstrap and paying particular attention to making the site responsive.
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// app.js is the main JS file which you should define your Angular module
+angular
+  .module('gin-app' );
+
+// ['ui.router', 'ngResource', 'satellizer', 'ui.bootstrap', 'ngAnimate', 'ngSanitize']
